2422de0ef469309f92c87ca35b06976992bb161b
jidapa <jidapa.p@kkumail.com>
2021-03-02 22:19:14 +0700

    [➕] add react-testing-library and jest

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const IndexPage: React.FC = () => {
     return (
         <div className="w-full h-screen flex items-center justify-center bg-purple-300 font-bold">
-            Hi next.js 🎉
+            <h1>Hi next.js 🎉</h1>
         </div>
     )
 }
