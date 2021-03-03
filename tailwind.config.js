986c250a02a10e6dfabd7506c31578c3fb4a80a2
jidapa <jidapa.p@kkumail.com>
2021-03-03 12:03:35 +0700

    [🍱] add assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@ module.exports = {
     purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
     darkMode: false, // or 'media' or 'class'
     theme: {
+        screens: {
+            sm: '480px',
+            md: '768px',
+            lg: '976px',
+            xl: '1440px'
+        },
+        colors: {
+            'black-50': 'hsla(0, 100%, 0%, 0.5)'
+        },
+        fontFamily: {
+            sans: ['IBM Plex Sans', 'sans-serif']
+        },
         extend: {}
     },
     variants: {
